41434f5bcf5bb1f20ba3d231903c7b00f557c050
mdridoyali <mdridoyislamr@gmail.com>
2023-12-22 19:22:37 +0600

    updated code

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const app = express()
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors')
 
-const app = express();
-const port = process.env.PORT || 5000;
 
-// middle were
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5174",
-      "http://localhost:5173",
-    //   "https://medicamp-603c8.web.app",
-    //   "https://medicamp-603c8.firebaseapp.com",
-    ],
-    credentials: true,
-  })
-);
-app.use(express.json());
+
+app.use(cors())
+app.use(express.json())
+
+
+// TaskFlow
+// ZHkB5dj64LnHx5MD
+
 
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASSWORD}@cluster0.w9fev91.mongodb.net/?retryWrites=true&w=majority`;
@@ -33,43 +28,56 @@ const client = new MongoClient(uri, {
 });
 
 
+const Tasks = client.db('task-manage').collection('Tasks')
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-        // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-        
-        const usersCollection = client.db('task-manage').collection("users");
-
-    app.post('/users', async(req, res)=>{
-        const user = req.body;
-        const result = await usersCollection.insertOne(user)
-        console.log(result)
-        res.send(result)
-    })
-    app.get('/users/:email', async (req, res) => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    app.get('/tasks/:email',async(req,res)=>{
         const email = req.params.email
-        // console.log(email)
-        const query = { email: email }
-        const result = await usersCollection.findOne(query);
-        res.send(result)
-      })
+        const query = {email : email}
+        const result = await Tasks.find(query).toArray()
+        res.send(result)       
+    })
 
 
+    app.post('/addtasks',async(req,res)=>{
+      console.log('task',req.body)
+      const body = req.body 
+      const result = await Tasks.insertOne(body)
+      res.send(result)   
 
+  })
+
+
+  app.put('/updateTask/:status/:id',async(req,res)=>{
+    const status = req.params.status
+    const Id = req.params.id
+    const query = { _id : new ObjectId(Id)  }
+    const updateDoc = {
+      $set:{
+        status : status
+      }
+    }
+    const result = await Tasks.updateOne(query,updateDoc)
+    res.send(result)
+    
+  })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    
   }
 }
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
-    res.send("Task manage is open");
-  });
-  
-  app.listen(port, () => {
-    console.log(`Task manage is open on port ${port}`);
-  });
+app.get('/', (req, res) => {
+  res.send('Task is ready')
+})
+
+app.listen(port, () => {
+  console.log(`Task is running on port ${port}`)
+})
